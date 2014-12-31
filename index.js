e0aa28a3014e84007b90ca39ef4ea16fff18c24f
Hiroki Osame <hirokio@bu.edu>
2014-12-31 20:56:51 +0900

    Code

---
+++ index.js
@@ -0,0 +1,70 @@
+module.exports = (function(){
+
+	'use strict';
+
+	function callN(counter, callback){
+		return function(){
+			if( !--counter ){ callback(); }
+		};
+	}
+
+	return function(options, master, worker){
+
+		var cluster = require('cluster'),
+			listen = require('processCallback.js');
+
+		// Master
+		if( cluster.isMaster ){
+
+			// Number of workers
+			var numWorkers = options,
+
+			// Store workers
+				workers = [];
+
+			// Wait until all workers are ready
+			var onlineCB = callN(numWorkers, function(){
+
+				// Invoke master
+				master(workers);
+			});
+
+			// When a Worker process is up
+			cluster.on('online', function( worker ){
+
+				// console.info('Worker ' + worker.process.pid + ' spawned' );
+
+				// Bind Listener to each forked process
+				workers.push( listen(worker) );
+
+				// Signal online
+				onlineCB();
+			});
+
+			// If a Worker process dies
+			cluster.on('exit', function(worker, code, signal){
+
+				// Delete worker
+				for( var i = 0; i<workers.length; i++ ){
+					if( workers[i].listeningTo === worker.process.pid ){
+						delete workers[i];
+						break;
+					}
+				}
+
+				// Respawn Worker
+				cluster.fork();
+			});
+
+			// Spawn workers
+			for( var i = 0; i < numWorkers; i++ ){
+				cluster.fork();
+			}
+		}
+
+		// Load worker
+		else {
+			listen(process, worker);
+		}
+	};
+})();
---
+++ test.js
@@ -0,0 +1,26 @@
+var clusterDistribution = require("./index");
+
+clusterDistribution(
+	require('os').cpus().length*4,
+	function master(workers){
+
+		console.log("Master online", workers.length);
+
+		var worker;
+		while( worker = workers.pop() ){ (function(worker){
+
+			// Send work
+			worker.send("Hello", function(res){
+				console.log("Response", res);
+
+				// Put back...
+				workers.push(worker);
+			});
+		})(worker); }
+	},
+	function worker(msg, toMaster){
+
+		// Process work and send back
+		toMaster(msg + " World");
+	}
+);
