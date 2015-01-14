3db34a1f90fbbd9a33d410ca6e21c0ea9d9abbcf
Hiroki Osame <hirokio@bu.edu>
2015-01-15 03:18:13 +0900

    Added strit to spawn

--- spawn.js
+++ spawn.js
@@ -1,13 +1,9 @@
-// try{
 
+'use strict';
 
-	var callback = eval("("+process.env.code+");"),
-		listen = require('processCallback.js');
+var callback = eval("("+process.env.code+");"),
+	listen = require('processCallback.js');
 
-	if( typeof callback === "function" ){
-		callback(listen(process));
-	}
-// }
-// catch(e){
-// 	console.log(e);
-// }
+if( typeof callback === "function" ){
+	callback(listen(process));
+}
